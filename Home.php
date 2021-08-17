870d54673aa10c9982840ac646c0f09569f42924
Seth <sstultz@lesley.edu>
2021-08-17 19:33:22 -0400

    3 commit Setting up pages adding photos

---
+++ Home.php
@@ -0,0 +1,36 @@
+<html>
+<header>
+<title>
+Seth Stultz
+</title>
+
+</head>
+<body>
+<div id="parent">
+
+	<div id="header">	
+	<h1>Welcome to My BLOG </h1>
+
+
+	<a href="Home Page.html">Home</a>
+	<a href="Post Page.html">Post Page</a>
+	<a href="New Post Submission Page.html">New Post Submission Page</a>
+	<div><!--close for header-->
+
+
+
+	<div id="content">
+		<img src="Images/Home.jpg" alt="Home" title="Home"class="Home"/>
+		<p class="text1">  Blog Page 
+		</p
+
+		
+	<div><!--close for content-->
+
+	<div id="footer">
+		&copy; Seth Stultz Blog Week 3 Data bases
+	<div><!--close for footer-->
+
+<div><!--close for parent-->
+</body>
+</html>
---
+++ New Post Submission Page.php
@@ -0,0 +1,30 @@
+<html>
+<header>
+<title>
+Seth Stultz
+</title>
+
+</head>
+<body>
+<div id="parent">
+
+	<div id="header">	
+	<h1>Welcome to My BLOG </h1>
+	<a href="Home Page.html">Home</a>
+	<a href="Post Page.html">Post Page</a>
+	<a href="New Post Submission Page.html">New Post Submission Page</a>
+	<div><!--close for header-->
+
+	<div id="content">
+		<img src="Images/New Post Submission Page.jpg" alt="New Post Submission Page" title="New Post Submission Page"class="Home"/>
+		<p class="text1">  Blog Page 
+		</p
+	<div><!--close for content-->
+
+	<div id="footer">
+		&copy; Seth Stultz Blog Week 3 Data bases
+	<div><!--close for footer-->
+
+<div><!--close for parent-->
+</body>
+</html>
